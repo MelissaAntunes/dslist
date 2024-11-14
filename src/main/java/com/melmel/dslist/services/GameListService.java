8ca61b04de279570f31310dded82c70be102aae9
Melissa <melissaantunesdk@gmail.com>
2024-11-14 15:31:29 -0300

    Move games on list

--- src/main/java/com/melmel/dslist/services/GameListService.java
+++ src/main/java/com/melmel/dslist/services/GameListService.java
@@ -2,7 +2,9 @@ package com.melmel.dslist.services;
 
 import com.melmel.dslist.dto.GameListDTO;
 import com.melmel.dslist.entities.GameList;
+import com.melmel.dslist.projections.GameMinProjection;
 import com.melmel.dslist.repositories.GameListRepository;
+import com.melmel.dslist.repositories.GameRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -15,9 +17,29 @@ public class GameListService {
     @Autowired
     private GameListRepository gameListRepository;
 
+    @Autowired
+    private GameRepository gameRepository;
+
     @Transactional(readOnly = true)
     public List<GameListDTO> findAll() {
         List<GameList> result = gameListRepository.findAll();
         return result.stream().map(x -> new GameListDTO(x)).toList();
+    }
+
+    @Transactional(readOnly = true)
+    public void move(Long listId, int sourceIndex, int destinationIndex) {
+
+        List<GameMinProjection> list = gameRepository.searchByList(listId);
+
+        GameMinProjection obj = list.remove(sourceIndex);
+        list.add(destinationIndex, obj);
+
+        int min = sourceIndex < destinationIndex ? sourceIndex : destinationIndex;
+        int max = sourceIndex < destinationIndex ? destinationIndex : sourceIndex;
+
+        for (int i = min; i <= max; i++) {
+            gameListRepository.updateBelongingPosition(listId, list.get(i).getId(), i );
+
+        }
     }
 }
